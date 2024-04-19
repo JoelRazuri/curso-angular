4824b9fda7488f2288e64daffb0b0c0b1e80a186
Joel Razuri <jrazuri98@hotmail.com>
2024-04-19 14:08:03 -0300

    componentes

--- curso-angular/src/app/app.component.ts
+++ curso-angular/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'curso-angular';
+  title = 'JOEL';
+  subtitle = ' la persona que esta logueada';
 }
--- curso-angular/src/app/app.module.ts
+++ curso-angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from './hijo/hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadreComponent,
+    HijoComponent
   ],
   imports: [
     BrowserModule,
