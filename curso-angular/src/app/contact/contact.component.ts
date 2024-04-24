06176316b395e8d50e1a0935b51993288246c3a6
Joel Razuri <jrazuri98@hotmail.com>
2024-04-23 23:44:44 -0300

    formularios

--- curso-angular/src/app/contact/contact.component.ts
+++ curso-angular/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  // Formulario tipo plantilla
+  // public user: any = {
+  //   name: '',
+  //   email: ''
+  // }
 
+  // enviar() {
+  //   console.log(this.user);
+  // }
+
+  // Formulario tipo reactivo
+  formContact: FormGroup;
+
+  constructor(private form: FormBuilder) {
+    this.formContact = this.form.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  enviar() {
+    console.log(this.formContact);
+  }
+
+  hasErrors(controlName: string, errorType: string) {
+    return this.formContact.get(controlName)?.hasError(errorType) && this.formContact.get(controlName)?.touched;
+  }
 }
